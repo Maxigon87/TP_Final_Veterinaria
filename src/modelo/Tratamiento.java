71049f5031f3010b533398d305f268d2a0bb438a
Gonzalez, Maximiliano Mario <100152911+Maxigon87@users.noreply.github.com>
2022-06-02 18:44:58 -0300

    Tratamiento
    Cambie solo el id_Tratamiento por idTratamiento

--- src/modelo/Tratamiento.java
+++ src/modelo/Tratamiento.java
@@ -5,15 +5,126 @@
  */
 package modelo;
 
+import java.util.Objects;
+
 /**
  *
- * @author Manuel Belgrano Nº3
+ * @author Maximiliano Gonzalez
  */
 public class Tratamiento {
-    int id_tratamiento;
+    int idTratamiento;
     String descripcion;
-    Visita visita;
+    Consulta consulta;
     double importe;
-    String tipo_tratamiento;
+    String tipoTratamiento;
     boolean activo;
+
+    public Tratamiento(int idTratamiento, String descripcion, Consulta consulta, double importe, String tipoTratamiento, boolean activo) {
+        this.idTratamiento = idTratamiento;
+        this.descripcion = descripcion;
+        this.consulta = consulta;
+        this.importe = importe;
+        this.tipoTratamiento = tipoTratamiento;
+        this.activo = activo;
+    }
+
+    public Tratamiento(String descripcion, Consulta consulta, double importe, String tipoTratamiento, boolean activo) {
+        this.descripcion = descripcion;
+        this.consulta = consulta;
+        this.importe = importe;
+        this.tipoTratamiento = tipoTratamiento;
+        this.activo = activo;
+    }
+
+    public Tratamiento() {
+    }
+
+    public int getId_tratamiento() {
+        return idTratamiento;
+    }
+
+    public void setId_tratamiento(int idTratamiento) {
+        this.idTratamiento = idTratamiento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Consulta getConsulta() {
+        return consulta;
+    }
+
+    public void setconsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public String getTipo_tratamiento() {
+        return tipoTratamiento;
+    }
+
+    public void setTipo_tratamiento(String tipo_tratamiento) {
+        this.tipoTratamiento = tipo_tratamiento;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Tratamiento{" + "id_tratamiento=" + idTratamiento + ", descripcion=" + descripcion + ", consulta=" + consulta + ", importe=" + importe + ", tipo_tratamiento=" + tipoTratamiento + ", activo=" + activo + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.consulta);
+        hash = 29 * hash + (int) (Double.doubleToLongBits(this.importe) ^ (Double.doubleToLongBits(this.importe) >>> 32));
+        hash = 29 * hash + Objects.hashCode(this.tipoTratamiento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tratamiento other = (Tratamiento) obj;
+        if (Double.doubleToLongBits(this.importe) != Double.doubleToLongBits(other.importe)) {
+            return false;
+        }
+        if (!Objects.equals(this.tipoTratamiento, other.tipoTratamiento)) {
+            return false;
+        }
+        if (!Objects.equals(this.consulta, other.consulta)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
 }
